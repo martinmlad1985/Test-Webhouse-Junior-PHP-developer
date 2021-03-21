f316efba224a7078a8f13a90b1cbf3d1dc5a62a4
martinmlad1985 <60547325+martinmlad1985@users.noreply.github.com>
2021-03-21 22:42:55 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
+
 import './main.sass'
-
-const content= document.querySelector('.content')
-
-const tap= document.getElementById('tap')
-tap.addEventListener('click', ()=>{
-	content.style.left= -100 + '%'
-})
-
-const cross= document.querySelector('.cross')
-cross.addEventListener('click', ()=>{
-	content.style.left= 0 + '%'
-})
-
 
   $( function() {
     $( ".datepicker" ).datepicker({
